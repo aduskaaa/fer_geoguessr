f93a309f90023d5dc6283e8b81afdd7a32aaa59d
aduskaaa <aduskaaaaaaaaa@gmail.com>
2026-05-06 17:56:37 +0200

    Add files via upload

--- assets/peer-service.js
+++ assets/peer-service.js
@@ -123,7 +123,22 @@ class PeerService extends EventTarget {
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
             { urls: 'stun:stun3.l.google.com:19302' },
-            { urls: 'stun:stun4.l.google.com:19302' }
+            { urls: 'stun:stun4.l.google.com:19302' },
+            {
+                urls: "turn:openrelay.metered.ca:80",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            },
+            {
+                urls: "turn:openrelay.metered.ca:443",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            },
+            {
+                urls: "turn:openrelay.metered.ca:443?transport=tcp",
+                username: "openrelayproject",
+                credential: "openrelayproject"
+            }
         ];
         const options = { 
             debug: 2, 
